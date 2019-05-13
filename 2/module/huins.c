7cbb831a1c0982f9b941af7ca27db7c8cda2dcdb
KOREAN139 <korean139@gmail.com>
2019-05-14 03:14:06 +0900

    <feat> add fpga component address

--- 2/module/huins.c
+++ 2/module/huins.c
@@ -5,9 +5,13 @@
 #include<linux/kernel.h>
 #include<linux/moudle.h>
 #include<linux/fs.h>
+#include<asm/io.h>
+#include<huins.h>
 
-#define DEVICE_NAME "dev_driver"
-#define MAJOR_NUM 242
+static unsigned char *dot_addr;
+static unsigned char *fnd_addr;
+static unsigned char *led_addr;
+static unsigned char *lcd_addr;
 
 static int huins_open(struct inode *inode,
                 struct file *file)
@@ -42,12 +46,20 @@ static int init_module()
                                 ret_val);
                 return ret;
         }
+	fnd_addr = ioremap(FND_ADDRESS, 0x4);
+	led_addr = ioremap(LED_ADDRESS, 0x1);
+	lcd_addr = ioremap(LCD_ADDRESS, 0x32);
+	dot_addr = ioremap(DOT_ADDRESS, 0x10);
         return 0;
 }
 
 void cleanup_module()
 {
         int ret;
+	iounmap(fnd_addr);
+	iounmap(led_addr);
+	iounmap(lcd_addr);
+	iounmap(dot_addr);
         ret = unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
         if (ret < 0)
                 printk(KERN_ALERT "Error: unregister_chrdev: %d\n", ret);
---
+++ 2/module/huins.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 Sanggu Han
+ */
+#ifndef _HUINS_H_
+#define _HUINS_H_
+
+#define DEVICE_NAME "dev_driver"
+#define MAJOR_NUM 242
+#define FND_ADDRESS 0x08000004
+#define LED_ADDRESS 0x08000016
+#define LCD_ADDRESS 0x08000090
+#define DOT_ADDRESS 0x08000210
+
+#endif
