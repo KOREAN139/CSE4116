21c25871fac1c5011c691c838371603b400e540e
KOREAN139 <korean139@gmail.com>
2019-05-16 04:52:41 +0900

    <feat> systemcall to wrapping option w/ given data
    returns -1 if given data is not valid
    returns 4byte stream option
    option structure is consist of 4 1byte sections, like below
    [POSITION|VALUE|INTERVAL|LAP]

---
+++ 2/kernel/sys_huinsw.c
@@ -0,0 +1,33 @@
+#include <linux/kernel.h>
+#include <asm/uaccess.h>
+
+#define RANGE_CHECK(X) (((X) > 0) && ((X) <= 100))
+#define TO_NTH_BYTE(X, N)       (((X) & 0xff) << ((sizeof(X) - (N)) * 8))
+#define CONSTRUCT_PARAM(P, V, I, L)     ((TO_NTH_BYTE(P, 1)) \
+                                        | (TO_NTH_BYTE(V, 2)) \
+                                        | (TO_NTH_BYTE(I, 3)) \
+                                        | (TO_NTH_BYTE(L, 4)))
+
+struct st_huins_op {
+        int interval, lap, op;
+};
+
+asmlinkage int sys_huinsw(struct st_huins_op *block)
+{
+        int i, pos, val;
+        struct st_huins_op input;
+
+        copy_from_user(&input, block, sizeof(struct st_huins_op));
+
+        if (!RANGE_CHECK(input.interval) || !RANGE_CHECK(input.lap))
+                return -1;
+
+        for (i = input.op, pos = 0; i && !(i % 10); i /= 10)
+                pos += 1;
+        val = i % 10;
+
+        if (pos > 3 || val > 8 || !val)
+                return -1;
+
+        return CONSTRUCT_PARAM(pos, val, input.interval, input.lap);
+}
