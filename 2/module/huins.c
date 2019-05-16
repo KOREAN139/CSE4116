6a2a8c3468b6201b7bc65d2b14cab052ffa6d750
KOREAN139 <korean139@gmail.com>
2019-05-16 19:29:42 +0900

    <fix> make timer work properly

--- 2/module/huins.c
+++ 2/module/huins.c
@@ -14,7 +14,6 @@
 #define UNIT_TIME (HZ / 10)
 
 static int device_open = 0;
-static int cnt;
 const char *student_num = "20151623";
 static const int num_len = 8;
 const char *student_name = "Sanggu Han";
@@ -25,10 +24,15 @@ static unsigned char *fnd_addr;
 static unsigned char *led_addr;
 static unsigned char *lcd_addr;
 
-static struct timer_list huins_timer;
-static int timer_op;
+static struct st_huins_timer {
+	struct timer_list timer;
+	int cnt;
+	int param;
+};
 
-static void huins_control_device(int pos, int val)
+struct st_huins_timer huins_timer;
+
+static void huins_control_device(int pos, int val, int cnt)
 {
         int i, indent;
         unsigned short int dot_val;
@@ -67,13 +71,15 @@ static void huins_clear_device(void)
                 outw(0, (unsigned int)dot_addr + i * 2);
         outw(0, (unsigned int)fnd_addr);
         outw(0, (unsigned int)led_addr);
-        for (i = 0; i < 32; i += 2)
-                outw(0, (unsigned int)lcd_addr + i);
+        for (i = 0; i < 32; i++)
+                outw(' ', (unsigned int)lcd_addr + i);
 }
 
 static void huins_run(unsigned long param)
 {
-       int op = *(int *)param;
+	struct st_huins_timer *t_data = (struct st_huins_timer *)param;
+       int op = t_data->param;
+       int cnt = t_data->cnt;
        int pos = POSITION(op);
        int val = VALUE(op);
        int gap = INTERVAL(op);
@@ -85,21 +91,20 @@ static void huins_run(unsigned long param)
        }
        lap -= 1;
 
-       huins_control_device(pos, val);
+       huins_control_device(pos, val, cnt);
 
        if (cnt && !(cnt % 8))
                pos = (pos - 1 + 4) % 4;
        val = val - 8 ? val + 1 : 1;
 
-       op = CONSTRUCT_PARAM(pos, val, gap, lap);
+       t_data->param = CONSTRUCT_PARAM(pos, val, gap, lap);
+       t_data->cnt += 1;
 
-       cnt += 1;
+       huins_timer.timer.function = huins_run;
+       huins_timer.timer.data = (unsigned long)&huins_timer;
+       huins_timer.timer.expires = get_jiffies_64() + (UNIT_TIME * gap);
 
-       huins_timer.function = huins_run;
-       huins_timer.data = (unsigned long)&op;
-       huins_timer.expires = get_jiffies_64() + (UNIT_TIME * gap);
-
-       add_timer(&huins_timer);
+       add_timer(&huins_timer.timer);
 }
 
 static int huins_open(struct inode *inode,
@@ -110,8 +115,6 @@ static int huins_open(struct inode *inode,
 
         device_open++;
 
-        cnt = 0;
-        timer_op = 0;
         huins_clear_device();
         
         try_module_get(THIS_MODULE);
@@ -131,11 +134,14 @@ static long huins_ioctl(struct file *file,
                 unsigned int ioctl_num,
                 unsigned long ioctl_param)
 {
+	int op;
         switch (ioctl_num) {
         case IOCTL_RUN_DEVICE:
-                copy_from_user(&timer_op,
-                                (void __user *)ioctl_param, sizeof(timer_op));
-                huins_run((unsigned long)&timer_op);
+                copy_from_user(&op,
+                                (void __user *)ioctl_param, sizeof(op));
+		huins_timer.cnt = 0;
+		huins_timer.param = op;
+                huins_run((unsigned long)&huins_timer);
                 break;
         }
 
@@ -162,7 +168,7 @@ int init_module()
 	led_addr = ioremap(LED_ADDRESS, 0x1);
 	lcd_addr = ioremap(LCD_ADDRESS, 0x32);
 	dot_addr = ioremap(DOT_ADDRESS, 0x10);
-        init_timer(&huins_timer);
+        init_timer(&huins_timer.timer);
         return 0;
 }
 
@@ -172,7 +178,7 @@ void cleanup_module()
 	iounmap(led_addr);
 	iounmap(lcd_addr);
 	iounmap(dot_addr);
-        del_timer_sync(&huins_timer);
+        del_timer_sync(&huins_timer.timer);
         unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
 }
 
